291ba7cefa82dcfef4e316fd6d9c74b4b6603dbd
datnguyendesign <ngtiendat.94.04@gmail.com>
2025-06-16 10:15:27 +0700

    add img

--- vite-project/src/Button.jsx
+++ vite-project/src/Button.jsx
@@ -3,6 +3,7 @@ function Button() {
     <>
       <label>
         Text input: <input name='myInput'/>
+        <img src="https://cdn-imgix.headout.com/tour/7064/TOUR-IMAGE/b2c74200-8da7-439a-95b6-9cad1aa18742-4445-dubai-img-worlds-of-adventure-tickets-02.jpeg?auto=format&w=900&h=562.5&q=90&ar=16%3A10&crop=faces%2Ccenter&fit=crop" />
       </label>
       <button className='btn' type='submit'>submit</button>
       
