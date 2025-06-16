db87019416f66cdeb78c2fd6b389cdabe2385403
datnguyendesign <ngtiendat.94.04@gmail.com>
2025-06-16 10:22:09 +0700

    disable input

--- vite-project/src/Button.jsx
+++ vite-project/src/Button.jsx
@@ -2,7 +2,7 @@ function Button() {
     return (
     <>
       <label>
-        Text input: <input name='myInput'/>
+        Text input: <input name='myInput' disabled/>
       </label>
       <button className='btn' type='submit'>submit</button>
       
