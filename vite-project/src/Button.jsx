c71903b16d221577f711f637ea6e16fd42e119ac
datnguyendesign <ngtiendat.94.04@gmail.com>
2025-06-16 10:05:06 +0700

    v0.0

---
+++ vite-project/src/Button.jsx
@@ -0,0 +1,13 @@
+function Button() {
+    return (
+    <>
+      <label>
+        Text input: <input name='myInput'/>
+      </label>
+      <button className='btn' type='submit'>submit</button>
+      
+    </>
+  )
+  
+}
+export default Button
