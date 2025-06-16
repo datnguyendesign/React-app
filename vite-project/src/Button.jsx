26926a867277b5d1f82538fa71d5a388b1955b0b
datnguyendesign <ngtiendat.94.04@gmail.com>
2025-06-16 10:28:14 +0700

    hidden

--- vite-project/src/Button.jsx
+++ vite-project/src/Button.jsx
@@ -2,8 +2,7 @@ function Button() {
     return (
     <>
       <label>
-        Text input: <input name='myInput'/>
-        <img src="https://cdn-imgix.headout.com/tour/7064/TOUR-IMAGE/b2c74200-8da7-439a-95b6-9cad1aa18742-4445-dubai-img-worlds-of-adventure-tickets-02.jpeg?auto=format&w=900&h=562.5&q=90&ar=16%3A10&crop=faces%2Ccenter&fit=crop" />
+        Text input: <input name='myInput' hidden/>
       </label>
       <button className='btn' type='submit'>submit</button>
       
